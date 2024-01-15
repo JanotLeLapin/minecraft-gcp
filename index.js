9e3b415f7bd810605e9c9e0413d3ccb92f4cefbc
Joseph DALY <joseph300905@gmail.com>
2024-01-15 16:53:03 +0100

    start server from bot

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import { Client } from "discord.js"
+import Compute from "@google-cloud/compute"
 
 const client = new Client({
   intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
+const compute = new Compute.v1.InstancesClient();
+
 client.on("ready", () => console.log("Ready"));
-client.on("messageCreate", msg => {
-  console.log(msg.content);
+client.on("messageCreate", async msg => {
+  if (msg.content === "server start") {
+    msg.reply("Démarrage du serveur...");
+    await compute.start({
+      project: "khadija-411118",
+      instance: "minecraft",
+      zone: "europe-west1-b",
+    });
+  }
 });
 
 client.login(process.env.TOKEN);
