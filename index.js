aa064f0c2df8dc832ab597a6e664a15d7c4568db
Joseph DALY <joseph300905@gmail.com>
2024-01-15 16:37:07 +0100

    login

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const client = new Client({
   intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
+client.on("ready", () => console.log("Ready"));
 client.on("messageCreate", msg => {
   console.log(msg.content);
 });
+
+client.login(process.env.TOKEN);
