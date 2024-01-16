5b2dc4764a25d5edb1cc101c0c7ce063bf39cf6d
Joseph DALY <joseph300905@gmail.com>
2024-01-16 02:03:24 +0100

    added stop function

--- index.js
+++ index.js
@@ -9,15 +9,36 @@ const client = new Client({
 
 const instances = new InstancesClient.v1.InstancesClient();
 
+const REQUEST = {
+  project: "khadija-411118",
+  instance: "minecraft",
+  zone: "europe-west1-b"
+};
+
+/**
+  * @returns {Promise<"PROVISIONING" | "STAGING" | "RUNNING" | "STOPPING" | "REPAIRING" | "TERMINATED" | "SUSPENDING" | "SUSPENDED">}
+  */
+const fetchStatus = async () => {
+  const instance = await instances.get(REQUEST).then(res => res[0]);
+  return instance.status;
+}
+
 client.on("ready", () => console.log("Ready"));
 client.on("messageCreate", async msg => {
   if (msg.content === "server start") {
+    const status = await fetchStatus();
+    switch (status) {
+      case "STAGING":
+        msg.reply("Le serveur est déjà en train de démarrer.");
+        return;
+      case "RUNNING":
+        msg.reply("Le serveur est actif.");
+        return;
+      default:
+        break;
+    }
     msg.reply("Démarrage du serveur...");
-    await instances.start({
-      project: "khadija-411118",
-      instance: "minecraft",
-      zone: "europe-west1-b"
-    });
+    await instances.start(REQUEST);
   } else if (msg.content === "server info") {
     let message;
     try {
@@ -28,6 +49,20 @@ client.on("messageCreate", async msg => {
       message = "Connexion au serveur impossible.";
     }
     msg.reply(message);
+  } else if (msg.content === "server stop") {
+    const status = await fetchStatus();
+    switch (status) {
+      case "STOPPING":
+        msg.reply("Le serveur a déjà été arrêté.");
+        return;
+      case "TERMINATED":
+        msg.reply("Le serveur est inactif.");
+        return;
+      default:
+        break;
+    }
+    msg.reply("Arrêt du serveur...");
+    await instances.stop(REQUEST);
   }
 });
 
